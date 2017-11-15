a7792cf79f838643b946d34ff55f9fe4fb4e3ff1
Anthony DELGADO <a.delgado@DIGINAMIC.LOCAL>
2017-11-15 14:53:37 +0100

    create parameters config

---
+++ Config/parameters.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: a.delgado
+ * Date: 10/11/2017
+ * Time: 10:58
+ */
+
+/**
+ * Website Parameters
+ */
+
+/*Website Title*/
+define("WEBSITE_TITLE","Empty Website");
+
+/**
+ * Website Database Parameters
+ */
+
+/*Compatible only with php 7 or higher*/
+define("DB",[
+    "DB_HOST_NAME" => "{{Your database host}}",
+    "DB_USER_NAME" => "{{Your login}}",
+    "DB_PASSWORD" => "{{Your password}}",
+    "DB_DATABASE" => "{{Your Database's name}}"
+]);
